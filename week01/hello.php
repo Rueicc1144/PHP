7d6e313596f24b3b10ed550cd688978b522b171f
Rueicc1144 <161005421+Rueicc1144@users.noreply.github.com>
2024-02-23 15:14:07 +0800

    Add files via upload

---
+++ week01/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+        echo "Hello php";
+        $a1 = "Hello" . "php";
+
+        echo "<br/>";
+        echo $a1;
+        ?>
+
+    </body>
+</html>
