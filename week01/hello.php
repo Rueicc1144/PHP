8c4c8523137e3ab81b8e5373df941d0e743600e6
Rueicc1144 <161005421+Rueicc1144@users.noreply.github.com>
2024-02-24 22:36:41 +0800

    Update hello.php to Kangroo Version

--- week01/hello.php
+++ week01/hello.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>澳洲袋鼠的故事</title>
     </head>
     <body>
         <?php
-        echo "Hello php";
-        $a1 = "Hello" . "php";
+        echo "<h2>Story    Of</h2>";
+        $a1 = "Australia " . "Kangaroo";
 
         echo "<br/>";
         echo $a1;
-        ?>
-
+        echo "<br/>";
+        ?>  
+    <img src="k_img/muscle-kangroo.jpg" width="15%"/>
+        <?php
+            echo "<p>你知道嗎？烏拉圭人口有345.7萬，</p>
+            <p>同時，僅澳大利亞就有4700萬只袋鼠。</p>
+            <p>如果袋鼠決定入侵烏拉圭，</p>
+            <p>那麽每一個烏拉圭人都要打14只袋鼠。</p>
+            <p>你不知道，你不在乎，你只關心你自己。</p>";
+            ?>
     </body>
 </html>
+
