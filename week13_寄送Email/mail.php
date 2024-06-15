3f027d70a150cf14d79e30bc722418c03fae266d
Rueicc1144 <161005421+Rueicc1144@users.noreply.github.com>
2024-06-15 20:09:33 +0800

    Add files via upload

---
+++ week13_寄送Email/mail.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+
+<form action="sendmail.php" method="POST">
+    請輸入收件人信箱<input type="email" name="to"><br>
+    輸入郵件標題<input type="text" name="subject"><br>
+    郵件內容<textarea rows="30" cols="30" name="content">
+        假追狗
+    </textarea><br>
+
+    <input type="submit">
+
+</form>
